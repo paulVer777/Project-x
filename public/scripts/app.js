07b2b8f43f2b651fab1801448882bd59f6302860
p <pawel.gitara@onet.eu>
2018-09-28 12:41:52 +0200

    using react, reactDOM and his method to render JSX to user screen

--- public/scripts/app.js
+++ public/scripts/app.js
@@ -4,18 +4,47 @@ console.log('app.js is running');
 
 // JSX - JavaScript XML
 
-var jssX = React.createElement(
-  'h1',
-  null,
-  ' Indecision App'
-);
+// var jssX =
+// (
+//      <div>
+//      < h1 > Indecision App</h1>
+//      <p>This is some information</p>
+//      <ol>1</ol>
+//      <ol>2</ol>
+//      </div>
+// )
+
 
 // var jssX = React.createElement(
 //     "h1",
 //     null,
 //     " This is jsx"
 //   );
+
 var boxForRender = document.querySelector('#app');
 
-ReactDOM.render(jssX, boxForRender); // render display jsx to the user screen, that method takes two arguments, first is jsx 
+// ReactDOM.render(jssX,boxForRender) 
+// render display jsx to the user screen, that method takes two arguments, first is jsx 
 // that is what we want to render and second is where we want to render.
+
+var templateTwo = React.createElement(
+    'div',
+    null,
+    React.createElement(
+        'h1',
+        null,
+        'Pawe\u0142 Polit'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Age:26'
+    ),
+    React.createElement(
+        'p',
+        null,
+        'Location: Lublin'
+    )
+);
+
+ReactDOM.render(templateTwo, boxForRender);
--- src/app.js
+++ src/app.js
@@ -2,14 +2,35 @@ console.log('app.js is running')
 
 // JSX - JavaScript XML
 
-var jssX = < h1 > Indecision App</h1>
+// var jssX =
+// (
+//      <div>
+//      < h1 > Indecision App</h1>
+//      <p>This is some information</p>
+//      <ol>1</ol>
+//      <ol>2</ol>
+//      </div>
+// )
+
 
 // var jssX = React.createElement(
 //     "h1",
 //     null,
 //     " This is jsx"
 //   );
+
 var boxForRender = document.querySelector('#app')
 
-ReactDOM.render(jssX,boxForRender) // render display jsx to the user screen, that method takes two arguments, first is jsx 
+// ReactDOM.render(jssX,boxForRender) 
+// render display jsx to the user screen, that method takes two arguments, first is jsx 
 // that is what we want to render and second is where we want to render.
+
+var templateTwo=(
+    <div>
+        <h1>Paweł Polit</h1>
+        <p>Age:26</p>
+        <p>Location: Lublin</p>
+    </div>
+)
+
+ReactDOM.render(templateTwo,boxForRender)
