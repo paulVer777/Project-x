a99ad6602eba5aa88a5cf7f958edcfb560d8dd2e
p <pawel.gitara@onet.eu>
2018-09-28 11:11:08 +0200

    basic setup, babel ( react and env plugin installed )

---
+++ public/scripts/app.js
@@ -0,0 +1,21 @@
+'use strict';
+
+console.log('app.js is running');
+
+// JSX - JavaScript XML
+
+var jssX = React.createElement(
+  'h1',
+  null,
+  ' Indecision App'
+);
+
+// var jssX = React.createElement(
+//     "h1",
+//     null,
+//     " This is jsx"
+//   );
+var boxForRender = document.querySelector('#app');
+
+ReactDOM.render(jssX, boxForRender); // render display jsx to the user screen, that method takes two arguments, first is jsx 
+// that is what we want to render and second is where we want to render.
