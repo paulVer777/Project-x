ebb1f1a20c0171c0771b075903022a4f908fa154
p <pawel.gitara@onet.eu>
2018-10-19 09:26:59 +0200

    modal styled

--- src/components/OptionModal.js
+++ src/components/OptionModal.js
@@ -1,17 +1,21 @@
 import React from 'react'
 import Modal from 'react-modal'
 
-
+ 
 const OptionModal = (props) => (
     <Modal
     isOpen={!!props.selectedOption}
     contentLabel='SelectedOption' // for people with disabilities
     onRequestClose={props.closeModal} // closes when user clicks on background or hit press escape button
-    
+    closeTimeoutMS={200}
+    className='modal' // modal will use our styles and get rid of default styles
     >
-    <h3>Selected option</h3>
-    {props.selectedOption&&<p>{props.selectedOption}</p>}
-    <button onClick={props.closeModal}>ok</button>
+    <h3 className='modal__title'>Selected option</h3>
+    {props.selectedOption&&<p className='modal__body'>{props.selectedOption}</p>}
+    <button 
+    className='button'
+    onClick={props.closeModal}>ok
+    </button>
     </Modal>
 )
 
